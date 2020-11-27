5f7a669e8deda5b8393e4014569d931da9f425e9
Feng-Shun Chang <fengshuc@ad.uci.edu>
2020-11-27 14:29:25 -0800

    Adding firestore support

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import firebase from './firebase/index';
+import AccountSearch from './component/AccountSearch';
 
 function App() {
   console.log(firebase.db);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AccountSearch />
     </div>
   );
 }
--- ui/src/firebase/index.js
+++ ui/src/firebase/index.js
@@ -1,9 +1,10 @@
-  import firebase from 'firebase'
-  import 'firebase/firestore'
+  import firebase from 'firebase/app';
+  import "firebase/auth";
+  import 'firebase/firestore';
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
+  const firebaseConfig = {
     apiKey: "AIzaSyCKvgb9gP_5Qu9Hc64p9JDRkS2BZ06y5kY",
     authDomain: "trustbook-a270b.firebaseapp.com",
     databaseURL: "https://trustbook-a270b.firebaseio.com",
@@ -12,13 +13,8 @@
     messagingSenderId: "183885774615",
     appId: "1:183885774615:web:abb27c025d39f2ca1c0152",
     measurementId: "G-QJ0NQ1NTDS"
-  };
-  // Initialize Firebase
+  };  // Initialize Firebase
+  
   firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-  let db = firebase.firestore();
-
-  export default {
-    firebase, db
-  }
+  
+  export default (firebase);
